dbfe3ee4489e6e16dac9b937523723d89e614dce
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 21:00:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def name=(dog_name)
-    this_dogs_name = dog_name # dog_name here is the argument
+    @this_dogs_name = dog_name # dog_name here is the argument
   end 
   
   def name 
-    this_dogs_name
+    @this_dogs_name
   end 
 end 
 
